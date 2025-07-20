18dc256aa66276ea282f39f44b668d577ebf8428
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-20 20:56:04 +0530

    ADded Desciption and Title

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,12 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white
      bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
-      
+      <p className=''>The Ultimate Hotel Experience</p>
+      <h1> Discover Your Perfect Gateway Destination</h1>
+      <p>
+        Explore our curated selection of hotels, from luxury resorts to cozy retreats. 
+        Book your dream stay today and create unforgettable memories.
+      </p>
     </div>
   )
 }
