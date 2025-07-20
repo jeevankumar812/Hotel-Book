c58f9bf014480d08e12ca4797d490ab8394aa816
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-20 21:54:36 +0530

    Navbar changed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 
+
+const BookIcon =()=>(
+    <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 const Navbar = () => {
   const navLinks = [
     { name: "Home", path: "/" },
@@ -15,6 +21,8 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const { openSignIn } = useClerk();
   const { user } = useUser();
+  const navigate=useNavigate();
+  const location=useLocation();
 
   React.useEffect(() => {
     const handleScroll = () => {
@@ -77,7 +85,14 @@ const Navbar = () => {
         />
 
         {user ? (
-          <UserButton afterSignOutUrl="/" />
+          <UserButton>
+            <UserButton.MenuItems>
+                <UserButton.Action label="My Bookings " labelIcon={
+                    <BookIcon/>
+                } onClick={()=>navigate('/my-bookings')
+                }/>
+            </UserButton.MenuItems>
+          </UserButton>
         ) : (
           <button
             onClick={() => openSignIn({})}
