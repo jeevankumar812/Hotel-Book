02678a99e24bf573532bd732ebbb9a0b4c37a084
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-24 09:12:39 +0530

    Updaing Exclusive offers

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const ExclusiveOffers = () => {
   return (
-    <div>
-      <div>
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
+      <div className='flex flex-col md:flex-row items-center justify-between w-full'>
         <Title />
-        <button>
+        <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12'>
             View All Offers
+            <img src={assets.arrowIcon} alt="arrow-icon" 
+            className='group-hover:translate-x-1 transition-all'/>
         </button>
       </div>
     </div>
