6cb6cbf461d4ffc952572ddef3e3649e442f3af4
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-24 11:39:33 +0530

    Creted Footer.jsx and Updating inside that file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
 import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <>
@@ -12,7 +13,7 @@ const Home = () => {
     <ExclusiveOffers/>
     <Testimonial/>
     <NewsLetter/>
-    
+    <Footer/>
     </>
   )
 }
