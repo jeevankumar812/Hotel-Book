28f889b276ea732076e85b82c8e31da55f3d894c
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-24 11:11:37 +0530

    Testimonial mouted

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Title from './Title'
 
 const Testimonial = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30'>
+      <Title title="What are Guest Say"
+      subTitle="We take pride in providing exceptional service
+       and creating memorable experiences for our guests. Here's what some
+        of them have to say about their stay with us." />
     </div>
   )
 }
