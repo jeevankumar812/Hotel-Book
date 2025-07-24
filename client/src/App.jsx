9b730cb592c0c4a2509afc9727e43478a212c93c
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-24 11:57:02 +0530

    updating AllRooms jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
+import AllRooms from './pages/AllRooms';
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
       <Routes>
         <Route  path='/' element={<Home/>}/>
+        <Route path='/rooms' element={<AllRooms/>}/>
       </Routes>
       </div>
     </div>
