405ca17f17beb97b39c64ed1b36e2bbc11a07476
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-24 08:53:45 +0530

    updating Featured Destination

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -6,7 +6,8 @@ import Title from './Title'
 const FeaturedDestination = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
-      <Title/>
+      <Title title='Featured Destination' subTitle='Discover our
+       handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences.'/>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
         {roomsDummyData.slice(0, 4).map((room, index) => (
           <HotelCard key={room._id} room={room} index={index} />
